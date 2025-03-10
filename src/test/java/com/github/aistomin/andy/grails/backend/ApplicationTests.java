5c2d91123f7c28e636973bfea630cd834a261d60
aistomin <andrej.istomin.ikeen@gmail.com>
2025-03-10 04:43:36 +0100

    Issue #1. The app generated by Spring Initializr

---
+++ src/test/java/com/github/aistomin/andy/grails/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aistomin.andy.grails.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
