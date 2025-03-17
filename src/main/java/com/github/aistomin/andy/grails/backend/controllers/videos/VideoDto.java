1ee4a090db75e8c66f2c43f6bf5dc4718279f3c1
aistomin <andrej.istomin.ikeen@gmail.com>
2025-03-17 13:28:03 +0100

    Issue #14. LICENSE file is fixed.

--- src/main/java/com/github/aistomin/andy/grails/backend/controllers/videos/VideoDto.java
+++ src/main/java/com/github/aistomin/andy/grails/backend/controllers/videos/VideoDto.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Istomin Andrej
+ * Copyright (c) 2025 Andrej Istomin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
