21ec8a5f4611518a2116c0687db2959e33f44fdb
aistomin <andrej.istomin.ikeen@gmail.com>
2025-08-13 17:34:12 +0200

    Issue #144. Use "docker compose" instead docker-compose.

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e  # Exit on any error
 
 echo "🐳 Building Docker image with no cache..."
-docker-compose build --no-cache
+docker compose build --no-cache
 
 echo "🚀 Starting services with docker-compose..."
-docker-compose up
+docker compose up
